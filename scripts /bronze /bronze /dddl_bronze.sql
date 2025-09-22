ca2d60dc28493679af480b23ddf817e742fbb19c
SrinidhiAdapa <srinidhiadapa2203@gmail.com>
2025-09-23 00:18:38 +0530

    Update dddl_bronze.sql

--- scripts /bronze /bronze /dddl_bronze.sql
+++ scripts /bronze /bronze /dddl_bronze.sql
@@ -1,4 +1,13 @@
-create database DataWareHouse;
+
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================create database DataWareHouse;
 
 use DataWareHouse;
 
